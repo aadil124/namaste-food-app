0c03747d268ff90aded9f53c5c79f2d486c272bd
aadil124 <ansariaadil124@gmail.com>
2023-10-30 22:56:03 +0530

    added filter card

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import objData from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+
+    const [filterData, setFilterData] = useState(objData)
+    const handleClick = () => {
+        let filterCardData = objData.filter((item) => {
+            // console.log(item?.info.avgRating)
+            return item?.info.avgRating > 3.8
+        })
+        setFilterData(filterCardData)
+    }
     return (
         <div className="body-container">
             <div className="search-container">
-                Search
+                <button onClick={handleClick}> Top Rated Restaurant</button>
             </div>
             <div className="cards-container">
                 {
-                    objData.map((restaurant) => {
+                    filterData.map((restaurant) => {
                         return <RestaurantCard restaurant={restaurant} key={restaurant.info.id} />
                     })
                 }
