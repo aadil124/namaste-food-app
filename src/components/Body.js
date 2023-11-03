4095ae8d9d5c36ad6d3d589391316deda4aa4fbb
aadil124 <ansariaadil124@gmail.com>
2023-11-03 21:40:15 +0530

    updated body components

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,8 @@ import ShimmerUI from "./ShimmerUI";
 
 const Body = () => {
 
-    const [restaurantData, setRestaurantData] = useState([])
+    const [listOfRestaurantData, setListOfRestaurantData] = useState([])
+    const [filteredRestaurantData, setFilteredRestaurantData] = useState([])
     const [searchText, setSearchText] = useState()
 
     useEffect(() => {
@@ -16,38 +17,40 @@ const Body = () => {
         const response = await data.json()
         const restaurantCardData = response?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
         // console.log(restaurantCardData[0].info)
-        setRestaurantData(restaurantCardData)
+        setListOfRestaurantData(restaurantCardData)
+        setFilteredRestaurantData(restaurantCardData)
     }
 
 
     const handleClick = () => {
-        let filterCardData = restaurantData.filter((item) => {
+        let filterCardData = listOfRestaurantData.filter((item) => {
             // console.log(item?.info.avgRating)
             return item?.info.avgRating > 3.8
         })
-        setRestaurantData(filterCardData)
+        setListOfRestaurantData(filterCardData)
+        setFilteredRestaurantData(filterCardData)
     }
 
     const searchTextButton = () => {
-        let searchFilteredRestaurant = restaurantData.filter((item) => {
+        let searchFilteredRestaurant = listOfRestaurantData.filter((item) => {
             return item?.info.name.toLowerCase().includes(searchText.toLowerCase())
         })
-        setRestaurantData(searchFilteredRestaurant)
+        setFilteredRestaurantData(searchFilteredRestaurant)
     }
 
 
-    return restaurantData.length === 0 ? (<ShimmerUI />) : (
+    return listOfRestaurantData.length === 0 ? (<ShimmerUI />) : (
         <div className="body-container">
             <div className="search-container">
                 <div className="search_box">
-                    <input type="text" placeholder="Search Here..." value={searchText} onChange={(e) => searchText === "" ? setRestaurantData(restaurantData) : setSearchText(e.target.value)} />
+                    <input type="text" placeholder="Search Here..." value={searchText} onChange={(e) => setSearchText(e.target.value)} />
                     <button onClick={searchTextButton}> Search Restaurant</button>
                 </div>
                 <button onClick={handleClick}> Top Rated Restaurant</button>
             </div>
             <div className="cards-container">
                 {
-                    restaurantData.map((restaurant) => {
+                    filteredRestaurantData.map((restaurant) => {
                         return <RestaurantCard restaurant={restaurant} key={restaurant.info.id} />
                     })
                 }
