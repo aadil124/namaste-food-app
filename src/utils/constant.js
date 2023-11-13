583853937820f4880c3ab82c6f0f03f9cfb331b7
aadil124 <ansariaadil124@gmail.com>
2023-11-13 12:15:53 +0530

    added constant variable

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,7 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
 export const LOGO_URL = "https://www.clipartmax.com/png/middle/111-1118804_android-food-delivery-apps.png"
+
+export const LIST_RESTAURANT_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2812547&lng=73.0482912&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
+export const REGULAR_MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.2812547&lng=73.0482912&restaurantId="
