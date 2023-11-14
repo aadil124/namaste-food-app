ccd9f4c48fac9b65a17b2411e6317238e7395f47
aadil124 <ansariaadil124@gmail.com>
2023-11-14 12:29:56 +0530

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
+import User from "./User"
+import UserClass from "./UserClass"
+
 const About = () => {
     return (
         <div>
             <h1>This is a About</h1>
+            {/* <User name={"Aadil function"} location={"Bhiwandi"} /> */}
+            <UserClass name={"Aadil Class"} location={"Bhiwandi"} />
         </div>
     )
 }
