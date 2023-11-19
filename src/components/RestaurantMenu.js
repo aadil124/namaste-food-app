62860b761f6cab9742c9d59dd61bc20d6b3e225b
aadil124 <ansariaadil124@gmail.com>
2023-11-19 12:18:59 +0530

    cleaning code

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,32 +1,14 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { REGULAR_MENU_URL } from "../utils/constant";
 import ShimmerUI from "./ShimmerUI"
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-
-    const [resInfo, setResInfo] = useState(null)
     const { resId } = useParams()
-    console.log(resId)
-
-    useEffect(() => {
-        fetchMenu();
-    }, [])
-
-    const fetchMenu = async () => {
-        const data = await fetch(`${REGULAR_MENU_URL}${resId}`)
-        const response = await data.json()
-        const menuCardInfo = response.data
-        setResInfo(menuCardInfo)
-        // console.log(menuCardInfo)
-    }
+    const resInfo = useRestaurantMenu(resId);
     if (resInfo === null) return <ShimmerUI />
-
-    // console.log(resInfo?.cards[0]?.card?.card?.info)
+    console.log(resInfo)
     const { name, avgRating, costForTwoMessage, cuisines, headerBanner, veg } = resInfo?.cards[0]?.card?.card?.info
-    // console.log(name)
-    // console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards)
-    const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
+    const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
     console.log(itemCards)
     return (
         <div className="menu">
@@ -39,9 +21,6 @@ const RestaurantMenu = () => {
                     itemCards.map((item) => { return <p key={item.card.info.id}>{item.card.info.name}</p> })
                 }
             </div>
-
-
-
         </div>
     )
 }
