8b537acf59b7d64cc46e583b1fc3f130d86380a7
aadil124 <ansariaadil124@gmail.com>
2023-11-20 19:54:27 +0530

    made new custom hook

---
+++ src/utils/useListOfRestaurant.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { LIST_RESTAURANT_URL } from "./constant";
+
+const useListOfRestaurant = () => {
+    const [listOfRestaurantData, setListOfRestaurantData] = useState([])
+    const [filteredRestaurantData, setFilteredRestaurantData] = useState([])
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+    const fetchData = async () => {
+
+        const data = await fetch(LIST_RESTAURANT_URL)
+        const response = await data.json()
+        const restaurantCardData = response?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        // console.log(restaurantCardData[0].info)
+        setListOfRestaurantData(restaurantCardData)
+        setFilteredRestaurantData(restaurantCardData)
+    }
+
+    return {
+        listOfRestaurantData,
+        filteredRestaurantData,
+        setListOfRestaurantData,
+        setFilteredRestaurantData
+    }
+}
+
+export default useListOfRestaurant;
