8a46964909d9036dacd832b4f99118fdde870621
aadil124 <ansariaadil124@gmail.com>
2023-11-30 12:58:36 +0530

    updated API

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,4 +4,6 @@ export const LOGO_URL = "https://www.clipartmax.com/png/middle/111-1118804_andro
 
 export const LIST_RESTAURANT_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2812547&lng=73.0482912&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-export const REGULAR_MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.2812547&lng=73.0482912&restaurantId="
+// export const REGULAR_MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.2812547&lng=73.0482912&restaurantId="
+
+export const REGULAR_MENU_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2891586&lng=73.0690544&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
