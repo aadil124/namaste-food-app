cca2678dccc13f14606354363a19413ac612a310
aadil124 <ansariaadil124@gmail.com>
2023-12-20 13:02:39 +0530

    added restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,26 +1,53 @@
 import { useParams } from "react-router-dom";
 import ShimmerUI from "./ShimmerUI"
 import useRestaurantMenu from "../utils/useRestaurantMenu";
-import { useEffect } from "react";
+import ItemsCategoryList from "./ItemsCategoryList";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const resInfo = useRestaurantMenu(resId);
   if (resInfo === null) return <ShimmerUI />;
-  console.log(resInfo);
+
+  const restaurantDetail = resInfo?.cards[0]?.card?.card?.info;
+  const ItemsCategoryDetails =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c?.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log(ItemsCategoryDetails);
   return (
-    <div className="menu">
-      <h1>Name of Restaurant: </h1>
-      {/* <p>{cuisines.join(", ")} - {costForTwoMessage}  </p>
-            <h3>Rating: {avgRating} </h3>
-            <div>
-                Menu:
-                {
-                    itemCards.map((item) => { return <p key={item.card.info.id}>{item.card.info.name}</p> })
-                }
-            </div> */}
+    <div className="my-6 w-6/12 mx-auto p-2">
+      <div className="flex justify-between border-b-2 border-dashed pb-3">
+        <div>
+          <p className="font-bold text-2xl text-left">
+            {restaurantDetail.name}
+          </p>
+          <p className="text-sm">{restaurantDetail.cuisines.join(", ")}</p>
+          <p className="text-sm">{restaurantDetail.areaName}</p>
+        </div>
+        <div className="border shadow rounded-md text-center p-2">
+          <h1 className="border-b-2 pb-1 text-sm">
+            <span className="text-sm p-1">⭐</span>
+            {restaurantDetail.avgRatingString}
+          </h1>
+          <h1 className="text-xs mt-2">
+            {restaurantDetail.totalRatingsString}
+          </h1>
+        </div>
+      </div>
+      <div>
+        {ItemsCategoryDetails.map((category) => {
+          return (
+            <ItemsCategoryList
+              key={category?.card?.card?.title}
+              category={category}
+            />
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default RestaurantMenu;
