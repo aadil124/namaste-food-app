225acf60a0faea2182f606810a3a79dc847999f8
aadil124 <ansariaadil124@gmail.com>
2023-12-22 11:59:08 +0530

    added resto menu

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -15,7 +15,6 @@ const useRestaurantMenu = (resId) => {
     const fetchData = async () => {
         const data = await fetch(REGULAR_MENU_URL + resId);
         const json = await data.json();
-        console.log(json)
         setResInfo(json.data)
     }
     return resInfo;
