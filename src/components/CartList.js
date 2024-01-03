06d290016b1cb0d25eaa0c5f515da4ea3b566cc0
aadil124 <ansariaadil124@gmail.com>
2024-01-03 21:17:22 +0530

    cart item list

---
+++ src/components/CartList.js
@@ -0,0 +1,14 @@
+import ItemList from "./ItemList";
+
+const CartList = () => {
+  return (
+    <>
+      <div className="w-6/12 m-auto text-center p-4 text-xl font-bold">
+        Cart
+        <div>{/* <ItemList ItemInfo /> */}</div>
+      </div>
+    </>
+  );
+};
+
+export default CartList;
