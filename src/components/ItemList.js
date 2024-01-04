c5fc4c2d839b3b9e2f3521c5641b4c7281729a0a
aadil124 <ansariaadil124@gmail.com>
2024-01-04 12:28:39 +0530

    added dispatch action on add button

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { CDN_URL } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/cartSlice";
 const ItemList = ({ ItemInfo }) => {
+  // console.log(ItemInfo);
+  const dispatch = useDispatch();
+  const handleAddItem = (item) => {
+    dispatch(addItem(item));
+  };
   return (
     <div>
       {ItemInfo.map((item) => (
-        <div className="flex justify-between border-b-2 p-2">
+        <div
+          className="flex justify-between border-b-2 p-2"
+          key={item?.card?.info?.id}
+        >
           <div className="w-9/12">
             <p className="font-medium">{item?.card?.info?.name}</p>
             <span className="text-sm">
@@ -22,7 +32,10 @@ const ItemList = ({ ItemInfo }) => {
               src={CDN_URL + item?.card?.info?.imageId}
               className="rounded-lg"
             />
-            <button className="absolute right-0 bottom-2 text-white bg-black p-1 rounded-md ">
+            <button
+              className="absolute right-0 bottom-2 text-white bg-black p-1 rounded-md"
+              onClick={() => handleAddItem(item)}
+            >
               Add +
             </button>
           </div>
