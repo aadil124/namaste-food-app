95dd8562dab9425bb57a4f7ad5e461da2bafa6b6
aadil124 <ansariaadil124@gmail.com>
2024-01-11 19:07:57 +0530

    test cases for restaurant card

---
+++ src/components/__tests__/RestaurantCard.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import RestaurantCard from "../RestaurantCard";
+import withPromotedCard from "../RestaurantCard";
+import MOCK_DATA from "../mocks/RestaurantMockData.json";
+import "@testing-library/jest-dom";
+
+it("Should load the restaurant component with Restaurant card", () => {
+  render(<RestaurantCard restaurant={MOCK_DATA} />);
+
+  const textName = screen.getByText("Domino's Pizza");
+
+  expect(textName).toBeInTheDocument();
+});
+
+// it("Should load the restaurant component with promoted label card", () => {
+//   const PromotedRestaurantCard = withPromotedCard(RestaurantCard);
+//   render(<PromotedRestaurantCard restaurant={MOCK_DATA} />);
+
+//   const textName2 = screen.getByText("Domino's Pizza");
+
+//   expect(textName2).toBeInTheDocument();
+// });
