a578761391062f9f0ae22dce98b62d7d6edb9b9d
aadil124 <ansariaadil124@gmail.com>
2024-01-23 23:25:55 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -2,37 +2,37 @@ import User from "./User"
 import UserClass from "./UserClass"
 import React from "react"
 
-// class About extends React.Component {
+class About extends React.Component {
 
-//     constructor(props) {
-//         super(props)
-//         console.log("parent constructor")
-//     }
-//     componentDidMount() {
-//         console.log("parent component Did Mount")
-//     }
-//     render() {
-//         console.log("parent render")
-//         return (
-//             <div>
-//                 <h1>This is a About</h1>
-//                 {/* <User name={"Aadil function"} location={"Bhiwandi"} /> */}
-//                 <UserClass name={"First "} location={"Bhiwandi"} />
-//                 <UserClass name={"Second "} location={"Bhiwandi"} />
-//                 <UserClass name={"Third "} location={"Bhiwandi"} />
-//             </div>
-//         )
-//     }
-// }
-
-const About = () => {
-    return (
-        <div>
-            <h1>This is a About</h1>
-            {/* <User name={"Aadil function"} location={"Bhiwandi"} /> */}
-            <UserClass name={"Aadil Class"} location={"Bhiwandi"} />
-        </div>
-    )
+    constructor(props) {
+        super(props)
+        console.log("parent constructor")
+    }
+    componentDidMount() {
+        console.log("parent component Did Mount")
+    }
+    render() {
+        console.log("parent render")
+        return (
+            <div>
+                <h1>This is a About</h1>
+                {/* <User name={"Aadil function"} location={"Bhiwandi"} /> */}
+                <UserClass name={"First "} location={"Bhiwandi"} />
+                <UserClass name={"Second "} location={"Bhiwandi"} />
+                <UserClass name={"Third "} location={"Bhiwandi"} />
+            </div>
+        )
+    }
 }
+
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>This is a About</h1>
+//             {/* <User name={"Aadil function"} location={"Bhiwandi"} /> */}
+//             <UserClass name={"Aadil Class"} location={"Bhiwandi"} />
+//         </div>
+//     )
+// }
 
 export default About
