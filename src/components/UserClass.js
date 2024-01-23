0116ddd05eb194f739bf8a891402a4fd42d3b62d
aadil124 <ansariaadil124@gmail.com>
2024-01-23 23:27:57 +0530

    update user class

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -10,33 +10,37 @@ class UserClass extends React.Component {
         this.state = {
             count: 0,
         }
-        console.log(this.props.name + "child constructor")
+        // console.log(this.props.name + "child constructor")
     }
     componentDidMount() {
-        console.log(this.props.name + "child component Did Mount")
+        // console.log(this.props.name + "child component Did Mount")
     }
 
     render() {
-        const { name, location } = this.props
-        const { count, count2 } = this.state
+      const { name, location } = this.props;
+      const { count, count2 } = this.state;
 
-        console.log(this.props.name + "child render")
-        return (
-            <div className="user_card">
-                <div className="user_items">
-                    <h1>Count : {count}</h1>
-                    <button onClick={() => {
-                        this.setState({
-                            count: this.state.count + 1,
-                        })
-                    }}>Increment </button>
-                    <h2>User Class</h2>
-                    <h3>Name: {name} </h3>
-                    <h3>Location: {location} </h3>
-                    <TestClass />
-                </div>
-            </div>
-        )
+      // console.log(this.props.name + "child render")
+      return (
+        <div className="user_card">
+          <div className="user_items">
+            <h1>Count : {count}</h1>
+            <button
+              onClick={() => {
+                this.setState({
+                  count: this.state.count + 1,
+                });
+              }}
+            >
+              Increment{" "}
+            </button>
+            <h2>User Class</h2>
+            <h3>Name: {name} </h3>
+            <h3>Location: {location} </h3>
+            <TestClass />
+          </div>
+        </div>
+      );
     }
 }
 
