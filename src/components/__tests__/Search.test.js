a9de764a21769cb92d13a93c99cdf508fa06865b
aadil124 <ansariaadil124@gmail.com>
2024-01-23 23:30:44 +0530

    integration testing

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -6,37 +6,40 @@ import { act } from "react-dom/test-utils";
 import MOCK_DATA from "../mocks/mockApiRestaurantCardData.json";
 import { BrowserRouter } from "react-router-dom";
 
-it("should load body component with search button ", async () => {
-  global.fetch = jest.fn(() => {
-    return Promise.resolve({
-      json: () => {
-        return Promise.resolve(MOCK_DATA);
-      },
-    });
+
+
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
   });
-
-  await act(async () => {
-    render(
-      <BrowserRouter>
-        <Body />
-      </BrowserRouter>
-    );
-  });
-
-  const searchBtn = screen.getByRole("button", { name: "Search Restaurant" });
-
-  expect(searchBtn).toBeInTheDocument();
 });
 
-it("should load body component with search button ", async () => {
-  global.fetch = jest.fn(() => {
-    return Promise.resolve({
-      json: () => {
-        return Promise.resolve(MOCK_DATA);
-      },
-    });
-  });
+// it("should load body component with search button ", async () => {
+//   global.fetch = jest.fn(() => {
+//     return Promise.resolve({
+//       json: () => {
+//         return Promise.resolve(MOCK_DATA);
+//       },
+//     });
+//   });
 
+//   await act(async () => {
+//     render(
+//       <BrowserRouter>
+//         <Body />
+//       </BrowserRouter>
+//     );
+//   });
+
+//   const searchBtn = screen.getByRole("button", { name: "Search Restaurant" });
+// //   const searchInput = screen.getByTestId("searchInput");
+
+//   expect(searchBtn).toBeInTheDocument();
+// });
+
+it("should load search Res list for pizza text input", async () => {
   await act(async () => {
     render(
       <BrowserRouter>
@@ -45,9 +48,19 @@ it("should load body component with search button ", async () => {
     );
   });
 
+  // const allCardsBeforeSearch = screen.getAllByTestId("resCard");
+  // expect(allCardsBeforeSearch.length).toBe(9);
+
   const searchBtn = screen.getByRole("button", { name: "Search Restaurant" });
+  const searchInput = screen.getByTestId("searchInput");
 
-  fireEvent.change({ name: "burger" });
+  fireEvent.change(searchInput, { target: { value: "pizza" } });
 
-  expect(searchBtn).toBeInTheDocument();
+  fireEvent.click(searchBtn);
+
+  // screen should load 2 cards
+
+  const allCardsAfterSearch = screen.getAllByTestId("resCard");
+
+  expect(allCardsAfterSearch.length).toBe(2);
 });
